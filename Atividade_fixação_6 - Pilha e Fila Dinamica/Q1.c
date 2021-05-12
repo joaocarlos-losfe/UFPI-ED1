a0a7cb1b4e8a21b7ba6ad2841233cdc2fb83d8fe
João <joaocarlos.losfe@gmail.com>
2021-05-11 21:16:03 -0300

    a questão 1 agora distribui entre as pilhas

--- Atividade_fixação_6 - Pilha e Fila Dinamica/Q1.c
+++ Atividade_fixação_6 - Pilha e Fila Dinamica/Q1.c
@@ -10,6 +10,7 @@ typedef struct
     char titulo[20];
     char artista[20];
     int ano;
+    char estilo_musical[10];
 
 }Artista;
 
@@ -39,6 +40,14 @@ int tamanhoPilha(Pilha* pilha_de_cds)
     return cont;
 }
 
+bool pilhaVazia(Pilha* pilha_de_cds)
+{
+    if(pilha_de_cds->topo_pilha == NULL)
+        return true;
+    else
+        return false;
+}
+
 void exibirPilhaCds(Pilha* pilha_de_cds)
 {
     int count = 1;
@@ -46,11 +55,12 @@ void exibirPilhaCds(Pilha* pilha_de_cds)
 
     while (end!=NULL)
     {
-        printf("\n\nCd: %d\n", count);
+        printf("\n\nCd: %d", count);
         printf("\nCodigo: %d", end->artista.codigo);
         printf("\nTitulo: %s", end->artista.titulo);
         printf("\nArtista: %s", end->artista.artista);
-        printf("\nAno: %d\n", end->artista.ano);
+        printf("\nAno: %d", end->artista.ano);
+        printf("\nEstilo musical: %s\n", end->artista.estilo_musical);
         
         count++;
         end = end->proximo_abaixo;
@@ -65,18 +75,83 @@ void empilharNovoCd(Pilha* pilha_de_cds, Artista cd)
     pilha_de_cds->topo_pilha = novo_cd;
 }
 
-bool pilhaVazia(Pilha* pilha_de_cds)
+
+void removerCd(Pilha* pilha_de_cds)
 {
-    if(pilha_de_cds->topo_pilha == NULL)
-        return true;
+    if(pilhaVazia(pilha_de_cds))
+        return;
     else
-        return false;
+    {
+        Pont apagar = pilha_de_cds->topo_pilha;
+        pilha_de_cds->topo_pilha = pilha_de_cds->topo_pilha->proximo_abaixo;
+        free(apagar);
+    }
+    
+}
+
+void separarDesempilharEmpilhar(Pilha *pilha_de_cds, Pilha *segunda_pilha, char estilo[10])
+{
+    Pilha auxliar;
+    auxliar.topo_pilha = NULL;
+
+    while (pilha_de_cds->topo_pilha !=NULL)
+    {
+
+        if(strcmp(pilha_de_cds->topo_pilha->artista.estilo_musical, estilo) == 0)
+        {
+            empilharNovoCd(segunda_pilha, pilha_de_cds->topo_pilha->artista);
+            printf("\neh pop\n");
+        }
+        else
+        {
+            empilharNovoCd(&auxliar, pilha_de_cds->topo_pilha->artista);
+            printf("\nnao eh pop\n");
+        }
+
+        Pont apagar = pilha_de_cds->topo_pilha;
+        
+        pilha_de_cds->topo_pilha = pilha_de_cds->topo_pilha->proximo_abaixo;
+
+        free(apagar);
+
+    }
+
+    while (auxliar.topo_pilha != NULL) // empilhar de volta os removidos na original exeto o disptribuido pra segunda pilha
+    {
+        empilharNovoCd(pilha_de_cds, auxliar.topo_pilha->artista);
+
+        auxliar.topo_pilha = auxliar.topo_pilha->proximo_abaixo;
+    }
+
+}
+
+void distibuirCds(Pilha *pilha_de_cds, int estilo, Pilha *pop, Pilha *rock, Pilha *sertanejo, Pilha *forro, Pilha *axe)
+{
+    switch (estilo)
+    {
+    case 1: //pop
+        break;
+    case 2: // rock
+        break;
+    case 3: // sertanejo
+        break;
+    case 4: // forro
+        break;
+    case 5: // axe
+        break;
+    default:
+        break;
+    }
 }
 
 int main()
 {    
     Pilha pilha_de_cds;
     pilha_de_cds.topo_pilha = NULL;
+
+    Pilha pop, rock, sertanejo, forro, axe;
+
+    pop.topo_pilha = rock.topo_pilha = sertanejo.topo_pilha = forro.topo_pilha = axe.topo_pilha = NULL; 
 
     int tamanho = tamanhoPilha(&pilha_de_cds);
     printf("%d", tamanho);
@@ -86,12 +161,41 @@ int main()
     strcpy(cd.artista, "Zedd");
     strcpy(cd.titulo, "Roses");
     cd.ano = 2015;
+    strcpy(cd.estilo_musical, "Pop");
+
 
     empilharNovoCd(&pilha_de_cds, cd);
-    empilharNovoCd(&pilha_de_cds, cd);
-    empilharNovoCd(&pilha_de_cds, cd);
+
+    cd.codigo = 222;
+    strcpy(cd.artista, "Avicci");
+    strcpy(cd.titulo, "Life");
+    cd.ano = 2014;
+    strcpy(cd.estilo_musical, "Rock");
     empilharNovoCd(&pilha_de_cds, cd);
 
+    cd.codigo = 333;
+    strcpy(cd.artista, "Martin garrix");
+    strcpy(cd.titulo, "friend");
+    cd.ano = 2017;
+    strcpy(cd.estilo_musical, "sertanejo");
+    empilharNovoCd(&pilha_de_cds, cd);
+    
+    //exibirPilhaCds(&pilha_de_cds);
+
+    /*
+    printf("\nremover cd topo...\n");
+    removerCd(&pilha_de_cds);
+    */
+
+    //exibirPilhaCds(&pilha_de_cds);
+
+    printf("\nseparando...\n");
+    char estilo[10];
+    strcpy(estilo, "Pop");
+    separarDesempilharEmpilhar(&pilha_de_cds, &pop, estilo);
+
+    exibirPilhaCds(&pop);
+    printf("\n\n");
     exibirPilhaCds(&pilha_de_cds);
 
     return 0;
