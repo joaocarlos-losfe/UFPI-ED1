96fa00bce5fa6449fd150ac62becd6797ff1f733
viktorsht <viktorshantus@gmail.com>
2021-05-17 22:04:24 -0300

    continuando os trabalhos

--- Trabalhos/Proj2/1.c
+++ Trabalhos/Proj2/1.c
@@ -8,9 +8,12 @@ uma saída, de tal forma que quando um determinado carro entra ele fica no final
 que chegou sempre fica próximo a saída, ou seja, todas as vezes que um carro sai todos os outros
 devem ser manobrados um espaço para frente. Faça um programa em C, onde o usuário entrará com o
 número da placa do carro e 'E' se estiver entrando no estacionamento e 'S' se estiver saindo do
-estacionamento. O programa deve emitir uma mensagem sempre que um carro entrar ou sair do
-estacionamento. Quando um carro chegar, a mensagem deve especificar se existe ou não vaga no
-estacionamento. Quando um carro sair, a mensagem deverá incluir o número de vezes em que o carro
+estacionamento.
+ O programa deve emitir uma mensagem sempre que um carro entrar ou sair do
+estacionamento.
+Quando um carro chegar, a mensagem deve especificar se existe ou não vaga no
+estacionamento.
+Quando um carro sair, a mensagem deverá incluir o número de vezes em que o carro
 foi manobrado para fora do estacionamento para permitir que outros carros saíssem, além de mostrar
 quantos carros foram manobrados para que ele saísse.
 Obs.: Não se esqueça que todas as vezes que um carro vai sair os que estão na frente devem ser
@@ -22,21 +25,21 @@ o primeiro da fila.
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define NCARRO 10
 
 void menu()
 {
 	printf("[0] - Sair do programa\n");
-	printf("[1] - Entrar no estacionamento\n");
-	printf("[2] - Sair do estacionamento\n");
+	printf("[1] - Continuar no estacionamento\n");
 	printf(": ");
 }
 
 typedef struct
 {
 	int placa_carro;
-	char status;
+	char status[1];
 }Carro;
 
 typedef struct
@@ -46,28 +49,65 @@ typedef struct
 }FilaCarro;
 
 int inserir(FilaCarro *fila, Carro carro);
-void cadastro(Carro *carro);
+int cadastro(Carro *carro);
+void imprimir(FilaCarro *fila);
+int filaVazia(FilaCarro *fila);
+
+int remover(FilaCarro *fila, Carro carro){
+	int c,i;
+	if(filaVazia(fila) != 0){
+		c = fila->carros[0];
+		for(i = 0; i < fila->fim; i++){
+			fila->carros[i] = fila->carros[i+1];
+		}
+		fila->fim--;
+	}
+	return c;
+}
 
 int main(int argc, char *argv[])
 {
 	FilaCarro fila;
 	Carro carro;
-	int op = 1;
+	int op;
 	do{
-		cadastro(&carro);
-		if(inserir(&fila,carro) == 0) printf("Carro adcionado!\n");
-		else printf("Fila cheia!\n");
+		menu();
+		scanf("%d",&op);
+		if(op == 0) exit(1);
+		else{
+			int r = cadastro(&carro);
+			if(r == 1)
+			{
+				if(inserir(&fila,carro) == 0) printf("Há vagas - Carro adcionado!\n");
+				else printf("Não há vagas!\n");
+			}
+			else
+			{
+				if(filaVazia(&fila) == 0) printf("Não há carros no estacionamento!\n");
+				else
+				{
+					printf("Há carro!\n");
+				}
+			}
+		}
+		imprimir(&fila);
+
 	}while(op != 0);
 	return 0;
 }
-void cadastro(Carro *carro)
+
+
+int cadastro(Carro *carro)
 {
+	int ret = 0; /*Se ret = 0 é pq o carro vai sair!*/
 	printf("Cadastro de carro!\n\n");
-	printf("Placa do carro: ");
+	printf("Placa do carro :: ");
 	scanf("%d",&carro->placa_carro);
-	printf("Entrar - e\nSair - s");
-	scanf("%c",carro->status);
+	printf("Entrar(e)	ou	Sair(s) :: ");
+	scanf("%s",carro->status);
 	printf("\n");
+	if(strcmp(carro->status,"e") == 0) ret = 1;
+	return ret;
 }
 
 int inserir( FilaCarro *fila, Carro carro)
@@ -83,4 +123,20 @@ int inserir( FilaCarro *fila, Carro carro)
 		fila->fim += 1;
 	}
 	return s;
+}
+
+void imprimir(FilaCarro *fila){
+  int i;
+  printf("imprimindo\n");
+  for(i = 0; i < fila->fim; i++){
+    printf("%d ",fila->carros[i].placa_carro);
+		//printf("%s",fila->carros[i].status);
+  }
+  printf("\n");
+}
+
+int filaVazia(FilaCarro *fila){
+	int vazio = 1;
+	if(fila->fim == 0) vazio = 0;
+	return vazio;
 }
