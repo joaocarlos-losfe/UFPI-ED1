106a0a92e64d6b5382f876108a14052a757f539c
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2021-05-18 15:26:55 -0300

    questão 3 não captura os ( ) ou o ultimo valor

--- Trabalhos/Proj2/Exer3.c
+++ Trabalhos/Proj2/Exer3.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <malloc.h>
+#include <locale.h>
 
 typedef struct 
 {
@@ -11,10 +12,27 @@ typedef struct
 
 }Expressao;
 
+typedef struct 
+{
+    char operadores[50];
+    int topo;
+
+}Operadores;
+
+typedef struct 
+{
+    int valores[50];
+    int topo;
+
+}Valores;
+
 void menu();
+void empilhar(char *str, Expressao *pilha);
 
 int main()
 {
+    setlocale(LC_ALL,"");
+
     Expressao expressao;
     expressao.topo = 0;
 
@@ -27,14 +45,14 @@ int main()
         menu();
         scanf("%d", &op);
         
-
         switch (op)
         {
         case 1:
-            printf("(str) digite um expressão: ")
-
+            setbuf(stdin, NULL);
+            printf("\ndigite uma expressão: ");
+            scanf("%[^\n]s", strexpressao);
+            empilhar(strexpressao, &expressao);
             break;
-        
         default:
             break;
         }
@@ -48,6 +66,71 @@ void menu()
 {
     printf("\n0 - sair");
     printf("\n1 - digitar expressão");
-    printf("\n2 - digitar expressão");
-    printf("\n3 - converter para pos fix\n: ");
+    printf("\n2 - mostrar expressão");
+    printf("\n3 - converter para pos fixa\n: ");
+}
+
+void empilhar(char *str, Expressao *pilha)
+{
+    char str_valor[5];
+    int i=0;
+
+    Operadores operadores;
+    operadores.topo = 0;
+
+    Valores valores;
+    valores.topo = 0;
+
+    while (*str!= '\0')
+    {
+        if(*str != ' ')
+        {
+            str_valor[i] = *str;
+            i++;
+        }
+        else
+        {
+            str_valor[i] = '\0';
+            i = 0;
+            
+            switch (str_valor[0])
+            {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '(':
+                case ')': operadores.operadores[operadores.topo] = str_valor[0];
+                operadores.topo ++;
+                break;
+
+                default:
+                    valores.valores[valores.topo] = atoi(str_valor);
+                    valores.topo++;
+                break;
+            }
+ 
+        }
+
+        ++str;
+    }
+    
+    valores.valores[valores.topo] = atoi(str_valor);
+    valores.topo++;
+
+    printf("\nmostrando as expressoes...\n");
+
+ 
+    for(i=operadores.topo -1; i>=0; i--)
+    {
+        printf("\n%c", operadores.operadores[i]);
+    }
+
+    printf("\nmostrando os valores...\n");
+
+    for(i=valores.topo -1; i>=0; i--)
+    {
+        printf("\n%d", valores.valores[i]);
+    }
+
 }
