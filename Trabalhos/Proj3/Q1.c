7c78cb1d1c4d2f19efd5b5687f181a34609efd16
viktorsht <viktorshantus@gmail.com>
2021-06-23 14:40:00 -0300

    função soma não funciona

--- Trabalhos/Proj3/Q1.c
+++ Trabalhos/Proj3/Q1.c
@@ -3,7 +3,6 @@
 #include<string.h>
 
 typedef struct{
-  char sinal;
   int coeficiente;
   char variavel;
   int expoente;
@@ -21,6 +20,71 @@ typedef struct{
 }Lista;
 
 void menu();
+void inserirFinal(Lista *lista, Termo t);
+void criar_lista(Lista *lista);
+void imprimir(Lista lista);
+void cadastroMonomio(Termo *t);
+
+void somaPolinomios(Lista *polinomio1, Lista *polinomio2){
+  printf("oi\n");
+  No *t1 = polinomio1->inicio;
+  No *t2 = polinomio2->inicio;
+  int i = 0;
+  printf("%d\n",t1->polinomio.coeficiente + t2->polinomio.coeficiente);
+}
+
+int main(int argc, char **argv[]){
+  Lista lista, *vetorPoli[100];
+  Termo t;
+  criar_lista(&lista);
+	int termos,op,contador = 0,p1,p2;
+	do{
+		menu();
+		scanf("%d",&op);
+		if(op == 0){
+			exit(1);
+		}
+		else if(op == 1){
+			printf("termos do polinomio:	");
+			scanf("%d",&termos);
+			for(int i = 0; i < termos; i++)
+			{
+				cadastroMonomio(&t);
+				inserirFinal(&lista,t);
+			}
+      vetorPoli[contador] = lista ;
+      contador++;
+		}
+    else if(op == 2){
+      if(lista.tam >= 2){
+        printf("Somar polinomios\n");
+        printf("Número do polinomio:	");
+        scanf("%d",&p1);
+        printf("Número do polinomio:	");
+        scanf("%d",&p2);
+        somaPolinomios(vetorPoli[p1],vetorPoli[p2]);
+      }
+      else{
+        printf("Para somar é preciso ter 2 ou mais polinômios\n");
+      }
+    }
+    else if(op == 3){
+      imprimir(lista);
+    }
+		else{
+			printf("erro de entrada!\n");
+		}
+	}while(op != 0);
+  return 0;
+}
+
+
+void menu(){
+	printf("[0] - Sair\n");
+	printf("[1] - Cadastrar Polinomio\n");
+  printf("[2] - Somar 2 polinomios\n");
+  printf("[3] - Imprimir\n");
+}
 
 void inserirFinal(Lista *lista, Termo t){
 	No *no,*novo = (No*)malloc(sizeof(No));
@@ -44,16 +108,16 @@ void criar_lista(Lista *lista){
   lista->tam = 0;
 }
 
-
 void imprimir(Lista lista){
 	No *no;
 	no = lista.inicio;
 	if(no != NULL){
     do{
-      printf("%c",no->polinomio.sinal);
       printf("%d",no->polinomio.coeficiente);
       printf("%c",no->polinomio.variavel);
-      printf("%d",no->polinomio.expoente);
+      if(no->polinomio.expoente != 0){
+        printf("%d",no->polinomio.expoente);
+      }
       no = no->proximo;
     }while(no != lista.inicio);
   }
@@ -65,9 +129,6 @@ void cadastroMonomio(Termo *t){
 	char sinal;
 	char variavel;
 	printf("Criar um Monomio\n");
-	printf("Sinal:	");
-	scanf(" %c",&t->sinal);
-	fflush(stdin);
 	printf("Coeficiente:	");
 	scanf(" %d",&t->coeficiente);
 	fflush(stdin);
@@ -77,45 +138,4 @@ void cadastroMonomio(Termo *t){
 	printf("Expoente:	");
 	scanf(" %d",&t->expoente);
 	fflush(stdin);
-
-}
-
-int main(int argc, char **argv[]){
-  Lista lista, vetorPoli[100];
-  Termo t;
-  criar_lista(&lista);
-	char *monomio;
-  monomio = (char*)malloc(sizeof(char) * 30);
-	int termos,op,contador = 0;
-	do{
-		menu();
-		scanf("%d",&op);
-		if(op == 0){
-			exit(1);
-		}
-		else if(op == 1){
-			printf("termos do polinomio:	");
-			scanf("%d",&termos);
-			for(int i = 0; i < termos; i++)
-			{
-				cadastroMonomio(&t);
-				inserirFinal(&lista,t);
-			}
-      vetorPoli[contador] = lista;
-      contador++;
-		}
-		else if(op == 2){
-			imprimir(lista);
-		}
-		else{
-			printf("erro de entrada!\n");
-		}
-	}while(op != 0);
-  return 0;
-}
-
-void menu(){
-	printf("[0] - Sair\n");
-	printf("[1] - Cadastrar Polinomio\n");
-  printf("[2] - Imprimir\n");
 }
